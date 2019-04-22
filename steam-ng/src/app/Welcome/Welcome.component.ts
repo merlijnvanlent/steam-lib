aedd835208fc6f0cf0eafb242cbadac0b5b6fe0c
Merlijn van Lent <merlijnvanlent@gmail.com>
2019-04-22 22:04:28 +0200

    routing and started with frondend

---
+++ steam-ng/src/app/Welcome/Welcome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './Welcome.component.html',
+  styleUrls: ['./Welcome.component.scss']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- steam-ng/src/app/app.module.ts
+++ steam-ng/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './Welcome/Welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
